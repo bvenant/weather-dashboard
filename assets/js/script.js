d2bcaa1f8319673be6bea7fdd2dc7620d74e5fd7
Bradley V <bvenant@aol.com>
2022-06-08 16:32:37 -0400

    added querySelectors to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+var cityFormEl = document.querySelector("#city-form");
+var cityEl = document.querySelector("#city");
+var currrentWeather = document.querySelector("#current-weather");
+var searchedCity = document.querySelector("#searched-city");
+var forecast = document.querySelector("#forecast");
+var cityFormEl = document.querySelector("#city-form");
+var cityFormEl = document.querySelector("#city-form");
+
 var apiKey = "92fb9e2a80553525912a61e5eeb8ce46";
